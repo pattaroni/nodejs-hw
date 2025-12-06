36c8532b503425fd07a173f6393df27ffd30e8a4
Oleksiy Kyryk <105497306+pattaroni@users.noreply.github.com>
2025-12-06 21:13:08 +0200

    Fix notesRoutes.js

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -9,7 +9,7 @@ import {
 import {
   createNoteSchema,
   getAllNotesSchema,
-  noteIdParamSchema,
+  noteIdSchema,
   updateNoteSchema,
 } from '../validations/notesValidation.js';
 import { celebrate } from 'celebrate';
@@ -18,11 +18,11 @@ const router = Router();
 
 router.get('/notes', celebrate(getAllNotesSchema), getAllNotes);
 
-router.get('/notes/:noteId', celebrate(noteIdParamSchema), getNoteById);
+router.get('/notes/:noteId', celebrate(noteIdSchema), getNoteById);
 
 router.post('/notes', celebrate(createNoteSchema), createNote);
 
-router.delete('/notes/:noteId', celebrate(noteIdParamSchema), deleteNote);
+router.delete('/notes/:noteId', celebrate(noteIdSchema), deleteNote);
 
 router.patch('/notes/:noteId', celebrate(updateNoteSchema), updateNote);
 
